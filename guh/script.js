47181b2b8e463dddda92f96d91f497cc9137bd10
czctus <goldenlandon7899@gmail.com>
2025-03-20 12:52:55 -0400

    Update script.js

--- guh/script.js
+++ guh/script.js
@@ -1,41 +1,41 @@
-var input = document.querySelector("input");
+var net;
+var button = document.querySelector("button");
 var pre = document.querySelector("pre");
-input.onkeydown = function (e) {
-  if (e.key === "Enter") magic();
+document.querySelector("input").onchange = function () {
+  if (this.length < 1) return;
+  var fr = new FileReader();
+  fr.readAsText(this.files.item(0));
+  fr.onload = function (e) {
+    var raw = JSON.stringify(e.target.result).split('\\"');
+    raw.forEach(function (e) {
+      if (
+        e.includes("IlR5cGUiOiAiV2lGaSIs") ||
+        e.includes("VHlwZSI6ICJXaUZpIiw") ||
+        e.includes("eXBlIjogIldpRmkiLA")
+      ) {
+        button.removeAttribute("disabled");
+        net = atob(e)
+          .replace(/\s\s+/g, " ")
+          .replace(/\\u([\d\w]{4})/gi, function (a, b) {
+            return String.fromCharCode(parseInt(b, 16));
+          })
+          .replace(/\\\\/g, "\\");
+        throw (pre.textContent = net
+          .split('"WiFi": {')
+          .join("youtu.be/dQw4w9WgXcQ")
+          .split('"AutoConnect"')
+          .join("youtu.be/dQw4w9WgXcQ")
+          .split("youtu.be/dQw4w9WgXcQ")
+          .filter(function (el, index) {
+            return index % 2 === 1;
+          })
+          .join("\n"));
+      } else button.disabled = true;
+      pre.textContent =
+        "no passwords were found; make sure you were importing the right file and have a DeviceOpenNetworkConfiguration in chrome://policy";
+    });
+  };
 };
-function magic() {
-  var value = input.value.trim();
-  if (!value.startsWith("{")) {
-    pre.textContent = `now go to chrome://sync-internals
-and click on the "search" tab
-and search for "wifi_"
-
-then click on:
-${value ? hex(value, true) : "the wifi of your choice"}
-
-then paste all the code that shows up into the input box
-and press the magic button`;
-  } else
-    try {
-      var json = JSON.parse(value);
-      pre.textContent = `data successfully found:
-
-wifi name: ${hex(json.NON_UNIQUE_NAME.split("<")[0])}
-wifi pass: ${atob(json.SPECIFICS.wifi_configuration.passphrase)}`;
-    } catch (e) {
-      pre.textContent = e.toString().toLowerCase();
-    }
-}
-function hex(wifi, way) {
-  return way
-    ? wifi
-        .split("")
-        .map((e) => e.charCodeAt(0).toString(16).padStart(2, "0"))
-        .join("")
-        .toUpperCase() + "<||>psk"
-    : wifi
-        .split(/(\w\w)/g)
-        .filter((e) => !!e)
-        .map((e) => String.fromCharCode(parseInt(e, 16)))
-        .join("");
+function raw() {
+  open().document.write(net);
 }
